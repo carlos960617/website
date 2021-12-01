1461fa18dd56f510476e2a8b7d1e79d6e2aaf5fd
Carlos Pastor <carlos960617@gmail.com>
2021-12-01 04:00:13 +0100

    cambio despues de guardar

--- index.php
+++ index.php
@@ -174,9 +174,6 @@
                     </button>
 
                     <!-- Topbar Search -->
-<<<<<<< HEAD
-                    <p>BIENVENIDO AL AREA PRINCIPAL</p>
-=======
                     <form
                         class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                         <div class="input-group">
@@ -189,7 +186,6 @@
                             </div>
                         </div>
                     </form>
->>>>>>> textoBuscador
 
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
